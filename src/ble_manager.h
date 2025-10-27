cab7bb112824f82770e9bc365c711c5368962c93
soerengraae <soerengraae@live.dk>
2025-10-27 21:51:47 +0100

    temp commit

--- src/ble_manager.h
+++ src/ble_manager.h
@@ -62,6 +62,9 @@ enum ble_cmd_type {
     /* Battery Service commands */
     BLE_CMD_BAS_DISCOVER,
     BLE_CMD_BAS_READ_LEVEL,
+
+    /* CSIP commands */
+    BLE_CMD_CSIP_DISCOVER,
 };
 
 /* BLE command structure */
@@ -97,6 +100,8 @@ int ble_cmd_vcp_read_flags(void);
 
 int ble_cmd_bas_discover(void);
 int ble_cmd_bas_read_level(void);
+
+int ble_cmd_csip_discover(void);
 
 void ble_manager_cmd_queue_reset(void);
 
